3f75b12bce81b0bdc9f6a22d6d37a7585212e871
Hayden <hayden.xiong@plaud.ai>
2025-12-27 21:23:29 +0800

    Update application.cc

--- main/application.cc
+++ main/application.cc
@@ -877,9 +877,12 @@ void Application::SetDeviceState(DeviceState state) {
             display->SetStatus(Lang::Strings::SPEAKING);
 
             if (listening_mode_ != kListeningModeRealtime) {
+                ESP_LOGI(TAG, "🎤 Speaking state: mic OFF (listening_mode=%d, not realtime)", listening_mode_);
                 audio_service_.EnableVoiceProcessing(false);
                 // Only AFE wake word can be detected in speaking mode
                 audio_service_.EnableWakeWordDetection(audio_service_.IsAfeWakeWord());
+            } else {
+                ESP_LOGI(TAG, "🎤 Speaking state: mic ON (listening_mode=%d, realtime mode)", listening_mode_);
             }
             audio_service_.ResetDecoder();
             break;
